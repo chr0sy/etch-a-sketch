00992093aa5b1ec292f38daa1897e4d59bc28209
Ted Reka <rekated97@gmail.com>
2022-12-25 23:57:09 +0100

    Experiments with making a grid.

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Hello")
+const container = document.getElementById('container')
+const square = document.createElement('div')
+square.classList.add('square');
+
+const gridSize = 40;
+
+for (let i = 0; i < gridSize; i++) {
+    container.appendChild(square.cloneNode(true));
+}
