1af5b272ef865541b440c9ebe7aeac54305f9a29
Ted Reka <rekated97@gmail.com>
2022-12-26 15:37:41 +0100

    Grid created 16x16 + trailing effect on hover. User changing grid size not working yet

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
-const container = document.getElementById('container')
-const square = document.createElement('div')
-square.classList.add('square');
+// Pixels per board
+let gridSize = 256;
+const cellSize = 32;
+let gridColumns = 16;
 
-const gridSize = 40;
+const container = document.getElementById('sketch-container')
+container.style.cssText += `grid-template-columns: repeat(${gridColumns}, ${cellSize}px [col-start]);\n`;
 
 for (let i = 0; i < gridSize; i++) {
-    container.appendChild(square.cloneNode(true));
+    const div = document.createElement('div');
+    div.style.cssText += `border: 1px solid black;\nheight: ${cellSize}px;\nwidth: ${cellSize}px;\n`;
+    container.appendChild(div);
+
+    // Change color on mouse-hover
+    div.addEventListener('mouseover', () => {
+        div.style.backgroundColor = 'red';
+    })
 }
+
+// User changing grid size
+
+const changeGridButton = document.getElementById('changeGrid');
+changeGridButton.addEventListener('click', () => {
+    const userAnswer = prompt('Please enter a grid size');
+    gridColumns = userAnswer;
+    gridSize = userAnswer * userAnswer;
+    console.log(gridColumns);
+    console.log(gridSize);
+})
