38cb248e42bfd31a4543010a49c920945268ff48
Ted Reka <rekated97@gmail.com>
2022-12-26 16:18:40 +0100

    Project done without the extra credit.

--- script.js
+++ script.js
@@ -1,29 +1,43 @@
-// Pixels per board
-let gridSize = 256;
-const cellSize = 32;
-let gridColumns = 16;
+let sketchWidth = 780;
 
-const container = document.getElementById('sketch-container')
-container.style.cssText += `grid-template-columns: repeat(${gridColumns}, ${cellSize}px [col-start]);\n`;
+function initialSketch(cols) {
+    let cells = sketchWidth / cols;
 
-for (let i = 0; i < gridSize; i++) {
-    const div = document.createElement('div');
-    div.style.cssText += `border: 1px solid black;\nheight: ${cellSize}px;\nwidth: ${cellSize}px;\n`;
-    container.appendChild(div);
+    const container = document.getElementById('sketch-container')
+    container.style.cssText += `grid-template-columns: repeat(${cols}, ${cells}px [col-start]);\nwidth: ${sketchWidth}px\n`;
 
-    // Change color on mouse-hover
-    div.addEventListener('mouseover', () => {
-        div.style.backgroundColor = 'red';
-    })
+    let gridSize = cols * cols;
+
+    // Clean container
+    container.innerHTML = ''
+
+    for (let i = 0; i < gridSize; i++) {
+        const div = document.createElement('div');
+        div.style.cssText += `border: 1px solid black;\nheight: ${cells}px;\nwidth: ${cells}px;\n`;
+        container.appendChild(div);
+
+        // Change color on mouse-hover
+        div.addEventListener('mouseover', () => {
+            div.style.backgroundColor = 'red';
+        })
+    }
 }
 
-// User changing grid size
+// Initialize first sketch with 16 cols
+initialSketch(16);
 
+// User changing grid size
 const changeGridButton = document.getElementById('changeGrid');
 changeGridButton.addEventListener('click', () => {
-    const userAnswer = prompt('Please enter a grid size');
-    gridColumns = userAnswer;
-    gridSize = userAnswer * userAnswer;
-    console.log(gridColumns);
-    console.log(gridSize);
+    let userPrompt = prompt('Please enter a grid size');
+    while (!/^(100|[1-9]?[0-9])$/.test(userPrompt)) {
+        alert('Please try again.');
+        userPrompt = prompt('Please enter a grid size');
+    }
+    initialSketch(userPrompt);
 })
+
+
+// PROBLEM:
+// Program creates a 16x16 grid by default first time and when it prompts
+// for user input, it just appends it - it does not replace it.
